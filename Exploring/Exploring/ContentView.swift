ec860af7bd319dd29938fd80c2d5cc8ba70df886
Elizabeth <e78hernandez@gmail.com>
2020-07-20 18:09:57 -0700

    first commit

---
+++ Exploring/Exploring/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Exploring
+//
+//  Created by Elizabeth Hernandez on 7/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
